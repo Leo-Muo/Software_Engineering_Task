61abb82ff4dc06e836bb3c229a842b597c519734
Leo-Muo <v.muoghara@gmail.com>
2021-02-04 01:32:29 +0200

    Inital project folders and files

---
+++ src/main/java/com/taskse/WebappTask/WebappTaskApplication.java
@@ -0,0 +1,13 @@
+package com.taskse.WebappTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappTaskApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/taskse/WebappTask/WebappTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskse.WebappTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
