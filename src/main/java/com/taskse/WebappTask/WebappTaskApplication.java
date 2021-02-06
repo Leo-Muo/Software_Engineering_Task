df65d91b50a9b7a3f3571c921edabad69b78f538
Leo-Muo <v.muoghara@gmail.com>
2021-02-06 02:00:18 +0200

    Created the landing page added: html css and image files

--- src/main/java/com/taskse/WebappTask/WebappTaskApplication.java
+++ src/main/java/com/taskse/WebappTask/WebappTaskApplication.java
@@ -2,6 +2,9 @@ package com.taskse.WebappTask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class WebappTaskApplication {
@@ -9,5 +12,4 @@ public class WebappTaskApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebappTaskApplication.class, args);
 	}
-	
 }
